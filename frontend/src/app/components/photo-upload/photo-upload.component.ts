633d3a2bd33fbba6c9ec4673657dfcaa192f6f34
johann-vu <68650868+johann-vu@users.noreply.github.com>
2023-08-24 12:38:03 +0200

    Refactoring File Upload

--- frontend/src/app/components/photo-upload/photo-upload.component.ts
+++ frontend/src/app/components/photo-upload/photo-upload.component.ts
@@ -1,37 +1,68 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-upload',
   templateUrl: './photo-upload.component.html',
   styleUrls: ['./photo-upload.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: PhotoUploadComponent,
+      multi: true,
+    }
+  ]
 })
-export class PhotoUploadComponent {
+export class PhotoUploadComponent implements ControlValueAccessor {
 
   files: File[] = [];
 
-  @Output() valueChange: EventEmitter<File[]> = new EventEmitter()
+  onChange = (x: any) => { console.log(x)  }
+  onTouched: any
+  touched = false;
+  disabled = false;
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  writeValue(v: File[]) {
+    this.files = v
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
 
   onFileSelect(event: any): void {
+    this.markAsTouched()
     this.files = []
     const fileList: FileList = event.target.files;
     for (let i = 0; i < fileList.length; i++) {
       const file = fileList[i];
       this.files.push(file)
     }
-    this.emitChange()
+    this.onChange(this.files)
   }
 
   onFileDelete(f: File) {
+    this.markAsTouched()
     if (!this.files) return
     let i = this.files.indexOf(f)
     if (i < 0) return
     this.files.splice(i, 1)
-    this.emitChange()
-  }
-
-  emitChange() {
-    console.log(this.files);
-    this.valueChange.emit(this.files)
+    this.onChange(this.files)
   }
 
 }
