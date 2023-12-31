9927829140fbac6a77cbe63ec2e3e358ca584e15
johann-vu <68650868+johann-vu@users.noreply.github.com>
2023-12-31 14:07:19 +0100

    Add Share Button on Success

--- frontend/src/app/pages/success/success.component.ts
+++ frontend/src/app/pages/success/success.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
   styleUrls: ['./success.component.scss']
 })
-export class SuccessComponent {
+export class SuccessComponent implements OnInit {
+  
+  ngOnInit(): void {
+    if (!navigator.share) return
+    this.supportsShare = true
+  }
 
+  supportsShare: boolean = false
+
+  async shareContent() {
+    const shareData = {
+      title: 'Online Möbellager',
+      text: 'Über das Online Möbellager kannst Du Möbel und Fahrräder spenden. Sieh es Dir doch mal an!',
+      url: 'https://online-mobellager.de'
+    };
+    navigator.share(shareData)
+  }
 }
