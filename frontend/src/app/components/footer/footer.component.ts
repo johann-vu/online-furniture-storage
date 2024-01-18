e4cd7bc4820189fbdcae5b93e0cf4f82036bc062
johann-vu <68650868+johann-vu@users.noreply.github.com>
2024-01-18 14:32:41 +0100

    Add Carbon Badge

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
 
+  @ViewChild('wcb') wcb: ElementRef | undefined;
+
+  ngAfterViewInit(): void {
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/website-carbon-badges@1.1.3/b.min.js';
+    script.defer = true;
+    this.wcb?.nativeElement.appendChild(script)
+  }
 }
